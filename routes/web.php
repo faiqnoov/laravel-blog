b5463901c96ec22e430170bc46478103118d2096
faiqnoov <mfaiqn@gmail.com>
2022-05-14 16:48:10 +0700

    #3 Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Faiq Novr",
+        "email" => "faiqnov@it.student.pens.ac.id",
+        "image" => "faiq.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

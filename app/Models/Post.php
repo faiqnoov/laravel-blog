3ef67907252946e9ad25c7ddd05b9149d14e094f
faiqnoov <mfaiqn@gmail.com>
2022-07-19 10:13:10 +0700

    #8 Post Category & Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     // Mass Assignment (2 cara, $fillable / $guarded)
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('container')
   <article>
     <h2>{{ $post->title }}</h2>
+    <p>By. Faiq Nov in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
     {{-- blade escape character --}}
     {!! $post->body !!}

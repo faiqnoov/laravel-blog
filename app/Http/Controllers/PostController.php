ba66b44e022685c0d41e6bd58de9fc3e1c319ebb
faiqnoov <mfaiqn@gmail.com>
2022-07-20 08:11:35 +0700

    #11 N+1 Problem

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,9 +10,9 @@ class PostController extends Controller
     public function index()
     {
         return view('posts', [
-            "title" => "Blog",
-            // "posts" => Post::all()
-            "posts" => Post::latest()->get()
+            "title" => "All Posts",
+            // Eager Loading
+            "posts" => Post::with(['author', 'category'])->latest()->get()
         ]);
     }
 

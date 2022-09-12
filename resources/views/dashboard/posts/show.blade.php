f24c747fcbb7fe462c78263255e5dbf67fe88c81
faiqnoov <mfaiqn@gmail.com>
2022-09-12 14:17:57 +0700

    #20.2 Update Post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,7 +6,7 @@
     <h2 class="mb-3">{{ $post->title }}</h2>
     
     <a href="/dashboard/posts" class="btn btn-sm btn-secondary"><span data-feather="arrow-left"></span> Back to all my posts</a>
-    <a href="" class="btn btn-sm btn-warning"><span data-feather="edit"></span> Edit</a>
+    <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-sm btn-warning"><span data-feather="edit"></span> Edit</a>
     <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
       @method('delete')
       @csrf

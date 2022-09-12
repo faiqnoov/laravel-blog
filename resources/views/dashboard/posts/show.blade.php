4b3972a6379a78300e7e1cb8d835965a77ed0b5e
faiqnoov <mfaiqn@gmail.com>
2022-09-12 14:17:28 +0700

    #20.1 Delete Post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,9 +7,13 @@
     
     <a href="/dashboard/posts" class="btn btn-sm btn-secondary"><span data-feather="arrow-left"></span> Back to all my posts</a>
     <a href="" class="btn btn-sm btn-warning"><span data-feather="edit"></span> Edit</a>
-    <a href="" class="btn btn-sm btn-danger"><span data-feather="x-circle"></span> Delete</a>
+    <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+      @method('delete')
+      @csrf
+      <button class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span> Delete</button>
+    </form>
 
-    <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid">
+    <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3">
 
     <article class="my-3 fs-5">
       {!! $post->body !!}
